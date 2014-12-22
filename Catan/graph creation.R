a2dee93f56277556797e56b2fcd3b11332881883
literateanalytics <pgowey@gmail.com>
2014-12-21 23:17:36 -0800

    Director and catan graphs

--- Catan/graph creation.R
+++ Catan/graph creation.R
@@ -1,10 +1,12 @@
-catan_results <- fread('/Users/pgowey/GitHub/Resources/Catan scripts/agg_2.txt')
+library(data.table)
+
+catan_results <- fread('/Users/pgowey/Analysis/Catan/agg_2.txt')
 
 # The palette with grey:
-#cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-cbPalette <- c("gray47", "darkorange4")
+# cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+cbPalette <- c('#009E73','#E69F00')
 # The palette with black:
-cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+# cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 # To use for fills, add
 #scale_fill_manual(values=cbPalette)
@@ -19,10 +21,35 @@ catan_results2 <- catan_results
 catan_results2$prod_dev <- round((catan_results$total_prod-catan_results$avg)/1000,4)
 catan_results2$test <- catan_results2$prod_dev >= 0
 
-ggplot(data=filter(catan_results2,draft_type=='Serpentine'),aes(x=player_pos,weight=prod_dev,fill=test))+
+####################################################################
+# main graph for blog post
+graph_data <- arrange(filter(catan_results2,draft_type=='Serpentine'),-player_pos)
+graph_data$player_pos <- factor(graph_data$player_pos,levels=c('Player4','Player3','Player2','Player1'))
+ggplot(data=graph_data,aes(x=player_pos,weight=prod_dev,fill=test))+
   geom_histogram()+
-  facet_wrap(~num_players,nrow=1)+
+  facet_wrap(~num_players,ncol=1)+
   scale_fill_manual(values=cbPalette,guide=FALSE)+
   labs(title='Resource-adjusted production power by party size')+
-  ylab('Player position in draft')+
-  xlab('Deviation from average')
+  ylab('Deviation from average')+
+  xlab('Player position in draft')+
+  theme(plot.title = element_text(size=18, face="bold", vjust=2,hjust=0))+
+  coord_flip()+
+#   ylim(-0.20,0.20)
+  ylim(min(graph_data$prod_dev)+(0.1*min(graph_data$prod_dev)),abs(min(graph_data$prod_dev)+(0.1*abs(min(graph_data$prod_dev)))))
+
+
+####################################################################
+# secondary graph for 2-player model
+two_p <- filter(catan_results2,num_players=='Two players')
+two_p$player_pos <- factor(two_p$player_pos,levels=c('Player2','Player1'))
+ggplot(data=two_p,aes(x=player_pos,weight=prod_dev,fill=test))+
+  geom_histogram()+
+  facet_wrap(~draft_type,ncol=1)+
+  scale_fill_manual(values=cbPalette,guide=FALSE)+
+  labs(title='Resource-adjusted production power in 2-player games')+
+  ylab('Deviation from average')+
+  xlab('Player position in draft')+
+  theme(plot.title=element_text(size=18,face='bold',vjust=2,hjust=0))+
+  coord_flip()#+
+#   ylim(min(two_p$prod_dev)+(0.2*min(two_p$prod_dev)),abs(min(two_p$prod_dev)+(0.2*abs(min(two_p$prod_dev)))))
+
--- Directors/directors graphing.R
+++ Directors/directors graphing.R
@@ -1,6 +1,8 @@
 library(ggplot2)
 library(dplyr)
 
+################################################################
+# import list of academy award winning or nominated directors
 directors <- read.table('/Users/pgowey/Analysis/Directors/directors.txt',sep=',',header=FALSE,stringsAsFactors=FALSE)
 names(directors)  <- c("wins","name","nominations")
 directors <- directors[,c(2,3,1)]
@@ -11,35 +13,83 @@ directors$name <- gsub('Miloš Forman','Milos Forman',directors$name)
 directors <- directors[-grep('Josh.*Logan.*',directors$name),]
 directors$rank <- rank(-directors$nominations,ties.method='random')
 
-
+################################################################
+# read full filmography of all directors, numerate year
 films <- read.table('/Users/pgowey/Analysis/directors/director_film_titles.txt',sep='\t',header=TRUE,quote='',fill=TRUE,stringsAsFactors=FALSE)
-
+films <- filter(films,is_film=='TRUE')
 films$year <- gsub('.*([0-9]{4}).*','\\1',films$release_date)
 films$year <- as.numeric(films$year)
 
+################################################################
+# aggregate director films and filter set to directors with median production year > 1960 (modern era)
 dir_agg <- summarise(group_by(films,director,year),avg_rating=mean(rating,na.rm=TRUE))
+dir_filter <- summarise(group_by(films,director),overall_rating=mean(rating,na.rm=TRUE),med_year=median(year,na.rm=TRUE)) %>% filter(med_year>1960) %>% select(director,overall_rating)
+dir_filter$overall_ranking <- rank(-dir_filter$overall_rating,ties.method='min')
+dir_agg <- inner_join(dir_agg,dir_filter,by=c('director'='director'))
 
-
-years<-c(1900:2014)
+################################################################
+# define years object for creating a complete timeline for all directors
+years <- c((min(dir_agg$year,na.rm=TRUE)):2014)
 years <- data.frame(years)
 names(years) <- c('year')
 class(years) <- c('data.frame')
-dir_years <- merge(years,select(directors,name,rank),all=TRUE)
+# using base package because dplyr has no full outer join option
+directors <- inner_join(directors,dir_filter,by=c('name'='director'))
+years <- merge(years,select(directors,name,rank,overall_ranking),all=TRUE)
 
+################################################################
+# define limits for top n directors, for now not excluding anyone
+# note... seems to be a problem with the dynamic upper bound. shouldn't use.
 m <- 0
-n <- 36
-
-dir_all_time <- left_join(years,dir_agg,by=c('year'='year'))
-dir_all_time <- left_join(dir_all_time,select(directors,name,rank),by=c('director'='name'))
-dir_top_12 <- filter(dir_all_time,rank > m & rank <= n)
-dir_top_12 <- left_join(filter(dir_years,rank > m & rank <= n),dir_top_12,by=c('year'='year','name'='director'))
-# head(filter(dir_all_time,is.na(director)=='FALSE'),20)
-
-# test graphing with one director
-# wyler <- filter(dir_all_time,director=='William Wyler')
-# wyler <- left_join(years,wyler,by=c('year'='year'))
-# qplot(data=wyler,x=year,weight=avg_rating,binwidth=1)
+n <- 24
+# n <- nrow(distinct(select(years,name)))
+years <- select(filter(years,overall_ranking > m & overall_ranking <= n),year,name,overall_ranking)
+# years <- select(years,year,name,overall_ranking)
+dir_top_n <- left_join(years,select(dir_agg,-overall_ranking),by=c('year'='year','name'='director'))
+# redefine name as a factor to influence ordering of facet grid later
+dir_top_n_reorder <- select(arrange(dir_top_n,overall_ranking),name)
+dir_top_n$name <- factor(dir_top_n$name,dir_top_n_reorder$name)
 
 
-# facet graph for the top 12
-qplot(data=dir_top_12,x=year,weight=avg_rating,binwidth=1)+facet_wrap(~name)+ylim(0,10)
+
+
+################################################################
+# start graphing
+
+# facet graphs for the top n defined above
+# limit should be 9.2 as this is the top score achieved by any film on IMDB
+# qplot(data=dir_top_12,x=year,weight=avg_rating,binwidth=1)+facet_wrap(~name)+ylim(0,9.2)+scale_fill_manual(values=cbPalette,guide=FALSE)
+
+# facet of dir avg ratings by year with trendline
+# ggplot(data=dir_top_n,aes(x=year,y=avg_rating,colour=avg_rating))+
+#   geom_point()+
+#   scale_colour_gradient(low="#EBF0E6", high="#142900",guide=FALSE)+
+#   ylim(min(dir_top_n$avg_rating)-0.1,max(dir_top_n$avg_rating)+0.1)+
+#   xlim(min(dir_top_n$year)-1,max(dir_top_n$year)+1)+
+#   facet_wrap(~name,ncol=4)+
+#   geom_smooth(method='loess',se=FALSE,colour='#000000')+
+#   labs(x='Year',y='Average Film Rating',title='IMDB Film Ratings for Top 24 Modern-Era Oscar Nominated Directors')+
+#   theme(plot.title = element_text(size=18, face="bold", vjust=2,hjust=0))
+
+# this is the final graph I used - slightly diff version from the one above.
+# shows all films instead of films agg'd to a year. 
+dir_top_n <- left_join(years,films,by=c('year'='year','name'='director'))
+dir_top_n_reorder <- select(arrange(dir_top_n,overall_ranking),name)
+dir_top_n$name <- factor(dir_top_n$name,dir_top_n_reorder$name)
+
+ggplot(data=dir_top_n,aes(x=year,y=rating,colour=rating))+
+  geom_point()+
+  scale_colour_gradient(low="#EAF3EE", high="#16452B",guide=FALSE)+
+#   scale_color_manual(values=c('#EBF0E6'),guide=FALSE)
+  ylim(min(dir_top_n$rating)-0.1,max(dir_top_n$rating)+0.1)+
+  xlim(min(dir_top_n$year)-1,max(dir_top_n$year)+1)+
+  facet_wrap(~name,ncol=4)+
+  geom_smooth(method='loess',se=FALSE,colour='seagreen')+
+  labs(x='Year',y='Average Film Rating',title='IMDB Film Ratings for Top 24 Modern-Era Oscar Nominated Directors')+
+  theme(plot.title = element_text(size=18, face="bold", vjust=2,hjust=0))
+
+
+# scatterplot directors by overall avg rating?
+# '#009E73','#E69F00'
+# scale_colour_gradient(low="#009E73", high="#E69F00",guide=FALSE)+
+# scale_colour_gradient(low="#EBF0E6", high="#142900",guide=FALSE)+
